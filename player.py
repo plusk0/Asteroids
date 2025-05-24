d3c7d4ddf96483081e9517f4a7837e548f849664
Stefan <plusko@pop-os.fritz.box>
2025-05-24 12:26:16 +0200

    changed player color to white, increased shooting speed

--- player.py
+++ player.py
@@ -20,7 +20,7 @@ class Player(CircleShape):
         return [a, b, c]
     
     def draw (self, screen):
-        pygame.draw.polygon(screen, 255, self.triangle(), 2)
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
